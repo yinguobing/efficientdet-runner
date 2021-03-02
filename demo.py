12252c305e0f44355052991f26ac8f7ce9a13899
yinguobing <yinguobing@gmail.com>
2021-03-02 19:48:06 +0800

    show FPS on screen

--- demo.py
+++ demo.py
@@ -1,5 +1,4 @@
 """Demo of running EfficientDet detection."""
-
 from argparse import ArgumentParser
 
 import cv2
@@ -23,8 +22,15 @@ if __name__ == '__main__':
     if not cap.isOpened():
         print('Error opening input video: {}'.format(args.video))
 
+    # Use meter to calculate FPS.
+    meter = cv2.TickMeter()
+
     # Capture frame-by-frame
     while cap.isOpened():
+        # Start the timmer.
+        meter.start()
+
+        # Read a frame.
         ret, frame = cap.read()
         if not ret:
             break
@@ -36,6 +42,9 @@ if __name__ == '__main__':
         # Run the model
         predictions = detector.predict(frame_raw, 0.4)
 
+        # Stop the timmer.
+        meter.stop()
+
         # Draw the bounding boxes. This is the MINIMAL code to show the
         # detection result. I believe you are able to draw the class names and
         # scores on the original frames.
@@ -43,6 +52,8 @@ if __name__ == '__main__':
         for box, score, class_ in zip(boxes, scores, classes):
             cv2.rectangle(frame, (int(box[1]), int(box[0])),
                           (int(box[3]), int(box[2])), (0, 255, 0), 2)
+        cv2.putText(frame, "FPS: {:.0f}".format(meter.getFPS()), (7, 14),
+                    cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
         # show the frame online, mainly used for real-time speed test.
         cv2.imshow('Frame', frame)
