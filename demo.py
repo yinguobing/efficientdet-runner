ffa24d7f14f1bddbd02b0771c944f2e9fb1efe18
yinguobing <yinguobing@gmail.com>
2021-03-03 15:56:49 +0800

    draw labels on top of the bounding boxes

--- demo.py
+++ demo.py
@@ -10,6 +10,91 @@ parser.add_argument("--video", type=str, default=None,
                     help="Video file to be processed.")
 args = parser.parse_args()
 
+# This is the label names for the default checkpoint file with COCO dataset.
+label_map = {
+    # 0: 'background',
+    1: 'person',
+    2: 'bicycle',
+    3: 'car',
+    4: 'motorcycle',
+    5: 'airplane',
+    6: 'bus',
+    7: 'train',
+    8: 'truck',
+    9: 'boat',
+    10: 'traffic light',
+    11: 'fire hydrant',
+    13: 'stop sign',
+    14: 'parking meter',
+    15: 'bench',
+    16: 'bird',
+    17: 'cat',
+    18: 'dog',
+    19: 'horse',
+    20: 'sheep',
+    21: 'cow',
+    22: 'elephant',
+    23: 'bear',
+    24: 'zebra',
+    25: 'giraffe',
+    27: 'backpack',
+    28: 'umbrella',
+    31: 'handbag',
+    32: 'tie',
+    33: 'suitcase',
+    34: 'frisbee',
+    35: 'skis',
+    36: 'snowboard',
+    37: 'sports ball',
+    38: 'kite',
+    39: 'baseball bat',
+    40: 'baseball glove',
+    41: 'skateboard',
+    42: 'surfboard',
+    43: 'tennis racket',
+    44: 'bottle',
+    46: 'wine glass',
+    47: 'cup',
+    48: 'fork',
+    49: 'knife',
+    50: 'spoon',
+    51: 'bowl',
+    52: 'banana',
+    53: 'apple',
+    54: 'sandwich',
+    55: 'orange',
+    56: 'broccoli',
+    57: 'carrot',
+    58: 'hot dog',
+    59: 'pizza',
+    60: 'donut',
+    61: 'cake',
+    62: 'chair',
+    63: 'couch',
+    64: 'potted plant',
+    65: 'bed',
+    67: 'dining table',
+    70: 'toilet',
+    72: 'tv',
+    73: 'laptop',
+    74: 'mouse',
+    75: 'remote',
+    76: 'keyboard',
+    77: 'cell phone',
+    78: 'microwave',
+    79: 'oven',
+    80: 'toaster',
+    81: 'sink',
+    82: 'refrigerator',
+    84: 'book',
+    85: 'clock',
+    86: 'vase',
+    87: 'scissors',
+    88: 'teddy bear',
+    89: 'hair drier',
+    90: 'toothbrush',
+}
+
 if __name__ == '__main__':
     # Summon a model runner.
     detector = Detector('saved_model')
@@ -44,13 +129,16 @@ if __name__ == '__main__':
         # Stop the timmer.
         meter.stop()
 
-        # Draw the bounding boxes. This is the MINIMAL code to show the
-        # detection result. I believe you are able to draw the class names and
-        # scores on the original frames.
+        # Draw the bounding boxes and the class names.
         boxes, scores, classes = predictions
         for box, score, class_ in zip(boxes, scores, classes):
-            cv2.rectangle(frame, (int(box[1]), int(box[0])),
-                          (int(box[3]), int(box[2])), (0, 255, 0), 2)
+            y0, x0, y1, x1 = [int(b) for b in box]
+            cv2.rectangle(frame, (x0, y0), (x1, y1), (0, 255, 0), 2)
+            cv2.putText(frame, "{}:{:.2f}".format(label_map[class_], score),
+                        (x0, y0-7), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0),
+                        1, cv2.LINE_AA)
+
+        # Draw FPS on the screen's top left corner.
         cv2.putText(frame, "FPS: {:.0f}".format(meter.getFPS()), (7, 14),
                     cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
